19731e2fd4ee843e4d3935d5399adc52cc891152
Michael Moser <michael.opper.moser@gmail.com>
2015-11-30 14:12:56 -0500

    Created Restaurant model

---
+++ BrewYou/app/src/main/java/com/tylerlutz/brewyou/Models/Restaurant.java
@@ -0,0 +1,103 @@
+package com.tylerlutz.brewyou.Models;
+
+import android.text.format.Time;
+
+import java.util.Date;
+import java.util.Calendar;
+
+/**
+ * Created by michaelmoser on 11/30/15.
+ */
+public class Restaurant {
+    private int restaurantId;
+    private String restaurantName;
+    private String restaurantAddress;
+    private String restaurantCity;
+    private String restaurantState;
+    private String restaurantZip;
+    private Date restaurantFirstVisit;
+    private Date restaurantLastVisit;
+
+    public Restaurant (){}
+
+    public Restaurant (String restaurantName, String restaurantAddress,
+                       String restaurantCity, String restaurantState,
+                       String restaurantZip){
+
+        this.restaurantName = restaurantName;
+        this.restaurantAddress = restaurantAddress;
+        this.restaurantCity = restaurantCity;
+        this.restaurantState = restaurantState;
+        this.restaurantZip = restaurantZip;
+        this.restaurantFirstVisit = Calendar.getInstance().getTime();
+    }
+
+    public int getRestaurantId(){
+        return restaurantId;
+    }
+
+    public void setRestaurantId(int restaurantId){
+        this.restaurantId = restaurantId;
+    }
+
+    public String getRestaurantName(){
+        return restaurantName;
+    }
+
+    public void setRestaurantName(String restaurantName){
+        this.restaurantName = restaurantName;
+    }
+
+    public String getRestaurantAddress(){
+        return restaurantAddress;
+    }
+
+    public void setRestaurantAddress (String restaurantAddress){
+        this.restaurantAddress = restaurantAddress;
+    }
+
+    public String getRestaurantCity(){
+        return restaurantCity;
+    }
+
+    public void setRestaurantCity(String restaurantCity){
+        this.restaurantCity = restaurantCity;
+    }
+
+    public String getRestaurantState(){
+        return restaurantState;
+    }
+
+    public void setRestaurantState(String restaurantState){
+        this.restaurantState = restaurantState;
+    }
+
+    public String getRestaurantZip(){
+        return restaurantZip;
+    }
+
+    public void setRestaurantZip(String restaurantZip){
+        this.restaurantZip = restaurantZip;
+    }
+
+    public Date getRestaurantFirstVisit(){
+        return restaurantFirstVisit;
+    }
+
+    public void setRestaurantFirstVisit(){
+        this.restaurantFirstVisit = Calendar.getInstance().getTime();
+    }
+
+    public Date getRestaurantLastVisit(){
+        if (restaurantLastVisit == null){
+            return restaurantFirstVisit;
+        }
+        else {
+            return restaurantLastVisit;
+        }
+    }
+
+    public void setRestaurantLastVisit(){
+        this.restaurantLastVisit = Calendar.getInstance().getTime();
+    }
+}
