5b27dfea249774ba1162631828dcfe5faaa9695f
Tyler Lutz <tlutz.computers@gmail.com>
2015-12-12 21:45:22 -0500

    Added placeholder layouts
    Added temporary layouts. Next going to add adapters and adding drink,

---
+++ BrewYou/app/src/main/java/com/tylerlutz/brewyou/BeerListActivity.java
@@ -0,0 +1,17 @@
+package com.tylerlutz.brewyou;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by Tyler on 12/12/15.
+ */
+public class BeerListActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_beer_list);
+
+
+    }
+}
