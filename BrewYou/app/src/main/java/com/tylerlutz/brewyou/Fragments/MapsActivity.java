5f691d593fdb4c270a00eb0f5cb27404b25731fa
Michael Moser <michael.opper.moser@gmail.com>
2015-12-14 11:19:09 -0500

    Made changes to the layout to display MapsActivity
    Corrected the syntax in the content_restraunant_details.xml file. Now
    it doesn't crash, but still doesn't display map.

--- BrewYou/app/src/main/java/com/tylerlutz/brewyou/Fragments/MapsActivity.java
+++ BrewYou/app/src/main/java/com/tylerlutz/brewyou/Fragments/MapsActivity.java
@@ -1,8 +1,7 @@
 package com.tylerlutz.brewyou.Fragments;
 
-import android.content.Intent;
-import android.support.v4.app.FragmentActivity;
 import android.os.Bundle;
+import android.support.v4.app.FragmentActivity;
 
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
@@ -18,13 +17,12 @@ import org.json.JSONException;
 public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
 
     private GoogleMap mMap;
-    private Intent intent;
     private Restaurant restaurant = new Restaurant();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_maps);
+        setContentView(R.layout.content_restaurant_details);
 
         try {
             restaurant.findRestaurantCoordinates();
